e13a137507c0b8620fa67a9ec2ae59bc08d05408
Aditya Sen <adityasen120@gmail.com>
2023-05-03 18:12:57 -0400

    change code to hand code, potentiometer control

--- RoboticHandAnim/RoboticHandAnim.ino
+++ RoboticHandAnim/RoboticHandAnim.ino
@@ -16,39 +16,18 @@ int endTurn = 180;
 int interval = 2;
 int delay_interval = 10;
 
+//init potentiometer
+int potentiometer_pin = 0;
+int value_poteniometer;
+
+
 //function to run each Servo
-void contractFinger(Servo fingerObject, bool directionOpposite = false) {
-  if (directionOpposite) {
-    // 180 to 0 degrees
-    for (int position = endTurn; position >= startTurn; position -= interval)
-    {
-      fingerObject.write(position);
-      delay(delay_interval); //milliseconds
-    }
-
-    // 0 to 180 degrees
-    for (int position = startTurn; position <= endTurn; position += interval)
-    {
-      fingerObject.write(position);
-      delay(delay_interval); //milliseconds
-    }
-  }
-
-  else {
-    // 0 to 180 degrees
-    for (int position = startTurn; position <= endTurn; position += interval)
-    {
-      fingerObject.write(position);
-      delay(delay_interval); //milliseconds
-    }
-
-    // 180 to 0 degrees
-    for (int position = endTurn; position >= startTurn; position -= interval)
-    {
-      fingerObject.write(position);
-      delay(delay_interval); //milliseconds
-    }
-  }
+void contractFingers(int value) {
+  fingerServos[0].write(value);
+  fingerServos[1].write(value);
+  fingerServos[2].write(value);
+  fingerServos[3].write(value);
+  fingerServos[4].write(value);
 }
 
 void setup() {
@@ -58,17 +37,16 @@ void setup() {
   fingerServos[3].attach(11);
   fingerServos[4].attach(12);
 
-  fingerServos[0].write(180);
+  fingerServos[0].write(0);
   fingerServos[1].write(0);
-  fingerServos[2].write(180);
+  fingerServos[2].write(0);
   fingerServos[3].write(0);
   fingerServos[4].write(0);
 }
 
 void loop() {
-  contractFinger(fingerServos[0], true); // index
-  contractFinger(fingerServos[1]); // middle
-  contractFinger(fingerServos[2], true); // ring
-  contractFinger(fingerServos[3]); // pinkie
-  contractFinger(fingerServos[4]); // thumb
+  value_poteniometer = analogRead(potentiometer_pin);
+  value_poteniometer = map(value_poteniometer, 0, 1023, 0, 180); //scale from 1023 to 180
+
+  contractFingers(value_poteniometer);
 }
